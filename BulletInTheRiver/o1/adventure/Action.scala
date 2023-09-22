dbad8f09b7eba00d52b19bd735493f389d952688
An Vu <80579213+anvusuomi@users.noreply.github.com>
2023-09-22 19:30:47 +0300

    Add files via upload

---
+++ BulletInTheRiver/o1/adventure/Action.scala
@@ -0,0 +1,35 @@
+package o1.adventure
+
+/** The class `Action` represents actions that a player may take in a text adventure game.
+  * `Action` objects are constructed on the basis of textual commands and are, in effect,
+  * parsers for such commands. An action object is immutable after creation.
+  * @param input  a textual in-game command such as “go east” or “rest” */
+class Action(input: String):
+
+  private val commandText = input.trim.toLowerCase
+  private val verb        = commandText.takeWhile( _ != ' ' )
+  private val modifiers   = commandText.drop(verb.length).trim
+
+  /** Causes the given player to take the action represented by this object, assuming
+    * that the command was understood. Returns a description of what happened as a result
+    * of the action (such as “You go west.”). The description is returned in an `Option`
+    * wrapper; if the command was not recognized, `None` is returned. */
+  def execute(actor: Player) = this.verb match
+    case "go"        => Some(actor.go(this.modifiers))
+    case "rest"      => Some(actor.rest())
+    case "xyzzy"     => Some("I know it's difficult to find the muderer, but you have to do it, it's a man life!")
+    case "quit"      => Some(actor.quit())
+    case "investigate" => Some(actor.investigate(modifiers))
+    case "get"       => Some(actor.get(modifiers))
+    case "drop"      => Some(actor.drop(modifiers))
+    case "inventory" => Some(actor.inventory)
+    case "help"      => Some(actor.help)
+    case "answer"    => Some(actor.mudererIs)
+    case other       => None
+
+
+  /** Returns a textual description of the action object, for debugging purposes. */
+  override def toString = s"$verb (modifiers: $modifiers)"
+
+end Action
+
